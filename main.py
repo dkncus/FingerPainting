b24fb231cb14c446afaea8bef7abb584374c16c5
David Kubala <dkubala@Davids-MacBook-Pro-2.local>
2022-05-07 02:50:53 +0100

    Progress

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ keypoint_classifier = KeyPointClassifier()
 with open('gesture_model/keypoint_classifier/keypoint_classifier_label.csv', encoding='utf-8-sig') as f:
     keypoint_classifier_labels = csv.reader(f)
     keypoint_classifier_labels = [row[0] for row in keypoint_classifier_labels]
+
+
 history_length = 16
 finger_gesture_history = deque(maxlen=history_length)
 
@@ -34,8 +36,15 @@ cap = cv.VideoCapture(0)
 past_time = 0
 current_time = 0
 circles = []
-num_past_gestures = 11
+num_past_gestures = 8
 past_gestures = []
+
+hsv_icon = cv.imread('assets/hsv_icons/hsv_icon_color.png')
+mask_icon = cv.imread('assets/hsv_icons/hsv_icon_mask.png')
+mask_icon = 1 - (mask_icon / 255).astype(np.uint8)
+
+cv.imshow('mask', mask_icon * 255)
+cv.imshow('hsv', hsv_icon)
 
 def interpret_frame(image):
     img_debug = image.copy()
@@ -64,44 +73,65 @@ def interpret_frame(image):
             landmark_list = calc_landmark_list(img_debug, hand_landmarks)
             pre_processed_landmark_list = pre_process_landmark(landmark_list)
             hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
+            past_gestures.append(hand_sign_id)
+            if len(past_gestures) >= num_past_gestures:
+                past_gestures.pop(0)
 
-
-            # if hand_sign_id == 2:
-            #     x_1, y_1 = landmark_list[8]
-            #     circles.append((x_1, y_1))
+            # Draw point marker circle
             x_1, y_1 = landmark_list[4]
             x_2, y_2 = landmark_list[8]
             x_c = int(round(((x_1 + x_2) / 2), 0))
             y_c = int(round(((y_1 + y_2) / 2), 0))
-            img_debug = cv.circle(img_debug, (x_c, y_c), 5, (0, 0, 255), thickness=2)
-            img_debug = cv.circle(img_debug, (x_c, y_c), 1, (0, 0, 255), thickness=1)
+            img_debug = cv.circle(img_debug, (x_c, y_c), 9, (0, 255, 255), thickness=2)
+            img_debug = cv.circle(img_debug, (x_c, y_c), 3, (0, 255, 255), thickness=1)
 
-            if hand_sign_id == 3:
+            # Palm sign
+            if hand_sign_id == 0:
+                x_1, y_1 = landmark_list[0]
+                x_2, y_2 = landmark_list[5]
+                x_3, y_3 = landmark_list[17]
+                x_c = int(round(((x_1 + x_2 + x_3) / 3), 0))
+                y_c = int(round(((y_1 + y_2 + y_3) / 3), 0))
+                img_debug = cv.circle(img_debug, (x_c, y_c), 10, (0, 255, 0), cv.FILLED)
+
+                # Paste image of color wheel at this location
+
+                s = 50
+                # Check if in image bounds
+                if not (x_c - s < 0 or x_c + s > w or y_c - s < 0 or y_c + s > h):
+                    color_mask = np.zeros_like(img)
+                    transparency_mask = np.zeros_like(img)
+
+                    color_mask[y_c - s:y_c + s, x_c - s:x_c + s, :] = hsv_icon
+                    transparency_mask[y_c - s:y_c + s, x_c - s:x_c + s, :] = mask_icon
+                    img_debug[transparency_mask == 1] = color_mask[transparency_mask == 1]
+
+
+
+            # Pinch sign
+            elif hand_sign_id == 3:
                 img_debug = cv.circle(img_debug, (x_1, y_1), 10, (0, 255, 0), cv.FILLED)
                 img_debug = cv.circle(img_debug, (x_2, y_2), 10, (0, 255, 0), cv.FILLED)
                 circles.append((x_c, y_c))
 
-            # # For each landmark in the set of landmarks
-            # for id, lm in enumerate(handLms.landmark):
-            #     # Scale the landmark position to match the screen
-            #     cx, cy, cz = int(lm.x * w), int(lm.y *h), lm.z
-            #
-            #     # Store the coordinates
-            #     coords[id-1] = np.array([cx, cy]).astype(np.int64)
-            #
-            #     # # Draw a circle at the thumb, index, and middle finger tips
-            #     if id == 4 or id == 8 or id == 12:
-            #         cv.circle(img_debug, (cx,cy), 10, (0,255,0), cv.FILLED)
-            #         cv.circle(img_debug_circles, (cx,cy), 10, (0,255,0), cv.FILLED)
             img_debug = draw_bounding_rect(use_brect=True, image=img_debug, brect=brect)
-        # Gesture Detector
 
     # Draw the computed circles on the screen
-    for circle in circles:
-        img_debug = cv.circle(img_debug, circle, 12, (255, 128, 0), cv.FILLED)
+    for i, circle in enumerate(circles):
+        img_debug = cv.circle(img_debug, circle, 15, (255, 255, 255), cv.FILLED)
 
+        if i < len(circles) - 1 and len(circles) > 2:
+            pass
+            # cv.line(img_debug, circle, circles[i+1], (255, 255, 255), thickness=5)
+
+
+
+    print(past_gestures)
     cv.imshow("Image", img_debug)
     cv.waitKey(1)
+
+
+
 
 def detect_gesture(coords, past_gestures):
     # Index finger keypoints
@@ -119,9 +149,6 @@ def detect_gesture(coords, past_gestures):
     # cv.line(img_debug, index_tips, middle_tips, (255, 255, 255), thickness=2)
 
     # Gesture 1 - Painting
-    if len(past_gestures) >= num_past_gestures:
-        past_gestures.pop(0)
-
     if (dist_lows / dist_tips) > 0.9 and (dist_mids / dist_tips) > 0.8:
         return 1
     else:
@@ -173,11 +200,9 @@ def calc_landmark_list(image, landmarks):
 
     return landmark_point
 
-
 def pre_process_landmark(landmark_list):
     temp_landmark_list = copy.deepcopy(landmark_list)
 
-    # 相対座標に変換
     base_x, base_y = 0, 0
     for index, landmark_point in enumerate(temp_landmark_list):
         if index == 0:
@@ -186,11 +211,9 @@ def pre_process_landmark(landmark_list):
         temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
         temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
 
-    # 1次元リストに変換
     temp_landmark_list = list(
         itertools.chain.from_iterable(temp_landmark_list))
 
-    # 正規化
     max_value = max(list(map(abs, temp_landmark_list)))
 
     def normalize_(n):

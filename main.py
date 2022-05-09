ee1c0e4b673fcf5dbe92bb50f1d5850a0dc9749b
David Kubala <dkubala@Davids-MacBook-Pro-2.local>
2022-05-09 17:41:41 +0100

    Color Wheel Selection and Line Segment Drawing

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ import csv
 from collections import deque
 import itertools
 import copy
-
+import math
 # Gesture Tracking Models
 from gesture_model import KeyPointClassifier
 from gesture_model import PointHistoryClassifier
+
+from scipy.ndimage import gaussian_filter1d
 
 class Interpreter():
 
@@ -38,9 +40,23 @@ class Interpreter():
         past_time = 0
         current_time = 0
         self.circles = []
-        self.num_past_gestures = 8
-        self.past_gestures = []
+        self.current_line_segment = []
+        self.line_segments = []
+        self.line_segment_colors = []
 
+        # The list of gestures in the past
+        self.past_gestures = []
+        self.num_past_gestures = 6
+
+        #
+        self.drawing_line_seg = False
+        self.num_line_segs = 0
+
+        #
+        self.current_color = (0, 0, 0)
+
+
+        # Icons drawn on the screen when open palm is shown
         self.hsv_icon = cv.imread('assets/hsv_icons/hsv_icon_color.png')
         self.mask_icon = cv.imread('assets/hsv_icons/hsv_icon_mask.png')
         self.mask_icon = 1 - (self.mask_icon / 255).astype(np.uint8)
@@ -63,20 +79,29 @@ class Interpreter():
 
         # print(results.multi_hand_landmarks)
         if results.multi_hand_landmarks:
+            # The gesture of each hand
+            hand_gesture_list = []
+            hand_landmarks_list = []
 
             # For each hand detected in the image
             for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
                 # Wireframe hand drawing
                 self.mpDraw.draw_landmarks(img_debug, hand_landmarks, self.mpHands.HAND_CONNECTIONS)
-                brect = self.calc_bounding_rect(img_debug, hand_landmarks)
+
+                # Calculate location of landmarks based on screen pos
                 landmark_list = self.calc_landmark_list(img_debug, hand_landmarks)
+
+                # Normalize the landmark list and detect hand gesture
                 pre_processed_landmark_list = self.pre_process_landmark(landmark_list)
                 hand_sign_id = self.keypoint_classifier(pre_processed_landmark_list)
+                hand_gesture_list.append(hand_sign_id)
+                hand_landmarks_list.append(landmark_list)
+
                 self.past_gestures.append(hand_sign_id)
                 if len(self.past_gestures) >= self.num_past_gestures:
                     self.past_gestures.pop(0)
 
-                # Draw point marker circle
+                # Drawing location guide circle
                 x_1, y_1 = landmark_list[4]
                 x_2, y_2 = landmark_list[8]
                 x_c = int(round(((x_1 + x_2) / 2), 0))
@@ -84,69 +109,136 @@ class Interpreter():
                 img_debug = cv.circle(img_debug, (x_c, y_c), 9, (0, 255, 255), thickness=2)
                 img_debug = cv.circle(img_debug, (x_c, y_c), 3, (0, 255, 255), thickness=1)
 
-                # Palm sign
-                if hand_sign_id == 0:
-                    x_1, y_1 = landmark_list[0]
-                    x_2, y_2 = landmark_list[5]
-                    x_3, y_3 = landmark_list[17]
-                    x_c = int(round(((x_1 + x_2 + x_3) / 3), 0))
-                    y_c = int(round(((y_1 + y_2 + y_3) / 3), 0))
-                    img_debug = cv.circle(img_debug, (x_c, y_c), 10, (0, 255, 0), cv.FILLED)
+                img_debug = self.single_hand_gesture_handler(hand_sign_id, landmark_list, img_debug, x_c, y_c)
 
-                    # Paste image of color wheel at this location
+            # If there are 2 hands in the image
+            if len(hand_gesture_list) == 2:
+                img_debug = self.double_hand_gesture_handler(hand_gesture_list, hand_landmarks_list, img_debug)
 
-                    s = 50
-                    # Check if in image bounds
-                    if not (x_c - s < 0 or x_c + s > w or y_c - s < 0 or y_c + s > h):
-                        color_mask = np.zeros_like(img_debug)
-                        transparency_mask = np.zeros_like(img_debug)
+        # Draw all other line segments
+        for i, line_segment in enumerate(self.line_segments):
+            # For each point in the set of points
+            for j, point in enumerate(line_segment):
+                if j < len(line_segment) - 1 and len(line_segment) > 2:
+                    cv.line(img_debug, point, line_segment[j+1], self.line_segment_colors[i], thickness=10)
 
-                        color_mask[y_c - s:y_c + s, x_c - s:x_c + s, :] = self.hsv_icon
-                        transparency_mask[y_c - s:y_c + s, x_c - s:x_c + s, :] = self.mask_icon
-                        img_debug[transparency_mask == 1] = color_mask[transparency_mask == 1]
+        # Draw current line segment
+        for i, point in enumerate(self.current_line_segment):
+            if i < len(self.current_line_segment) - 1 and len(self.current_line_segment) > 2:
+                cv.line(img_debug, point, self.current_line_segment[i + 1], self.current_color, thickness=10)
 
+        return img_debug
 
+    def single_hand_gesture_handler(self, hand_sign_id, landmark_list, img_debug, x_c, y_c):
+        # Get image information
+        image_debug = img_debug.copy()
+        h, w, c = image_debug.shape
 
-                # Pinch sign
-                elif hand_sign_id == 3:
-                    img_debug = cv.circle(img_debug, (x_1, y_1), 10, (0, 255, 0), cv.FILLED)
-                    img_debug = cv.circle(img_debug, (x_2, y_2), 10, (0, 255, 0), cv.FILLED)
-                    self.circles.append((x_c, y_c))
+        # Palm sign
+        if hand_sign_id == 0:
+            x_1, y_1 = landmark_list[0]
+            x_2, y_2 = landmark_list[5]
+            x_3, y_3 = landmark_list[17]
+            x_hand_c = int(round(((x_1 + x_2 + x_3) / 3), 0))
+            y_hand_c = int(round(((y_1 + y_2 + y_3) / 3), 0))
+            image_debug = cv.circle(image_debug, (x_hand_c, y_hand_c), 10, (0, 255, 0), cv.FILLED)
 
-                img_debug = self.draw_bounding_rect(use_brect=True, image=img_debug, brect=brect)
+            b = 135
+            if not (x_hand_c - b < 0 or x_hand_c + b > w or y_hand_c - b < 0 or y_hand_c + b > h):
+                image_debug = cv.circle(image_debug, (x_hand_c, y_hand_c - int(b*1.5)), color=self.current_color, radius=b//2, thickness=cv.FILLED)
 
-        # Draw the computed circles on the screen
-        for i, circle in enumerate(self.circles):
-            img_debug = cv.circle(img_debug, circle, 15, (255, 255, 255), cv.FILLED)
+            # Paste image of color wheel at this location
+            s = 50
 
-            if i < len(self.circles) - 1 and len(self.circles) > 2:
-                pass
-                cv.line(img_debug, circle, self.circles[i+1], (255, 255, 255), thickness=5)
+            # Check if in image bounds
+            if not (x_hand_c - s < 0 or x_hand_c + s > w or y_hand_c - s < 0 or y_hand_c + s > h):
+                color_mask = np.zeros_like(image_debug)
+                transparency_mask = np.zeros_like(image_debug)
 
-        print(self.past_gestures)
-        cv.imshow("Image", img_debug)
-        cv.waitKey(1)
+                color_mask[y_hand_c - s:y_hand_c + s, x_hand_c - s:x_hand_c + s, :] = self.hsv_icon
+                transparency_mask[y_hand_c - s:y_hand_c + s, x_hand_c - s:x_hand_c + s, :] = self.mask_icon
+                image_debug[transparency_mask == 1] = color_mask[transparency_mask == 1]
 
-    def detect_gesture(self, coords, past_gestures):
-        # Index finger keypoints
-        index_lows, middle_lows = (coords[5, 0], coords[5, 1]), (coords[9, 0], coords[9, 1])
-        index_mids, middle_mids = (coords[6, 0], coords[6, 1]), (coords[10, 0], coords[10, 1])
-        index_tips, middle_tips = (coords[7, 0], coords[7, 1]), (coords[11, 0], coords[11, 1])
+        # Detect if a line segment is being drawn
+        if hand_sign_id == 3 and self.past_gestures.count(3) > self.num_past_gestures - 4:
+            self.drawing_line_seg = True
 
-        # Geometric distances between points
-        dist_tips = np.sqrt(np.sum(np.square(np.abs(np.array(index_tips) - np.array(middle_tips)))))
-        dist_mids = np.sqrt(np.sum(np.square(np.abs(np.array(index_mids) - np.array(middle_mids)))))
-        dist_lows = np.sqrt(np.sum(np.square(np.abs(np.array(index_lows) - np.array(middle_lows)))))
+        # If there is currently a line segment being drawn
+        if self.drawing_line_seg:
+            self.current_line_segment.append((x_c, y_c))
 
-        # cv.line(img_debug, index_lows, middle_lows, (255, 255, 255), thickness=2)
-        # cv.line(img_debug, index_mids, middle_mids, (255, 255, 255), thickness=2)
-        # cv.line(img_debug, index_tips, middle_tips, (255, 255, 255), thickness=2)
+            if self.past_gestures.count(3) <= self.num_past_gestures - 4:
+                self.current_line_segment = self.current_line_segment[:len(self.current_line_segment) - 6]
+                self.line_segments.append(self.current_line_segment)
+                self.current_line_segment = []
+                self.line_segment_colors.append(self.current_color)
+                self.num_line_segs += 1
+                self.drawing_line_seg = False
 
-        # Gesture 1 - Painting
-        if (dist_lows / dist_tips) > 0.9 and (dist_mids / dist_tips) > 0.8:
-            return 1
-        else:
-            return 0
+        return image_debug
+
+    def double_hand_gesture_handler(self, hand_gesture_list, landmark_list, img_debug):
+        image_debug = img_debug.copy()
+
+        # If right or left hand is pointing while other is showing palm
+        if (hand_gesture_list[0] == 0 and hand_gesture_list[1] == 4) \
+                or (hand_gesture_list[0] == 4 and hand_gesture_list[1] == 0):
+
+            # Check which hand is pointing
+            if hand_gesture_list[0] == 4:
+                pointer_index = 0
+            else:
+                pointer_index = 1
+
+            # X, Y location of the pointer point
+            x_point, y_point = landmark_list[pointer_index][8]
+
+            # X, Y location of the center of the hand
+            x_hand_1, y_hand_1 = landmark_list[1-pointer_index][0]
+            x_hand_2, y_hand_2 = landmark_list[1-pointer_index][5]
+            x_hand_3, y_hand_3 = landmark_list[1-pointer_index][17]
+            x_hand = int(round(((x_hand_1 + x_hand_2 + x_hand_3) / 3), 0))
+            y_hand = int(round(((y_hand_1 + y_hand_2 + y_hand_3) / 3), 0))
+
+            image_debug = cv.line(image_debug, color=(255, 128, 0), pt1=(x_hand, y_hand), pt2=(x_point, y_point))
+
+            d_y = x_point - x_hand
+            d_x = y_point - y_hand
+
+            d = math.sqrt(((d_x ** 2) + (d_y ** 2)))
+
+            if d < 50:
+                theta = math.atan2(d_y, d_x)
+                theta = math.degrees(theta) + 180
+                theta = theta - 135
+
+                if theta < 0:
+                    theta = theta + 360
+
+                rgb = self.hsv_to_rgb(theta/360, 1, 1)
+                print(rgb)
+
+                rgb = ( int(rgb[0] * 255),
+                        int(rgb[1] * 255),
+                        int(rgb[2] * 255))
+
+                self.current_color = rgb
+
+                print(d, theta)
+        return image_debug
+
+    def hsv_to_rgb(self, h, s, v):
+        if s == 0.0: return (v, v, v)
+        i = int(h * 6.)  # XXX assume int() truncates!
+        f = (h * 6.) - i
+        p, q, t = v * (1. - s), v * (1. - s * f), v * (1. - s * (1. - f))
+        i %= 6
+        if i == 0: return (v, t, p)
+        if i == 1: return (q, v, p)
+        if i == 2: return (p, v, t)
+        if i == 3: return (p, q, v)
+        if i == 4: return (t, p, v)
+        if i == 5: return (v, p, q)
 
     def calc_bounding_rect(self, image, landmarks):
         image_width, image_height = image.shape[1], image.shape[0]
@@ -164,7 +256,6 @@ class Interpreter():
         x, y, w, h = cv.boundingRect(landmark_array)
 
         return [x, y, x + w, y + h]
-
 
     def draw_bounding_rect(self, use_brect, image, brect):
         # Draw the image using the bounding rectangle
@@ -229,8 +320,12 @@ class Interpreter():
 
             # If an image from the webcam comes across
             if success:
+
                 # Interpret it
-                self.interpret_frame(img)
+                debug_image = self.interpret_frame(img)
+
+                cv.imshow('debug', debug_image)
+                cv.waitKey(1)
 
 
 if __name__ == '__main__':
